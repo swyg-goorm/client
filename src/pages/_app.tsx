8002dee07f758a0077beac64083ff1cc985033fd
Kuesung Park <gueit214@naver.com>
2023-02-13 10:16:45 +0900

    feat : favicon 추가 & title 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,16 @@ import '../styles/globals.css';
 import { RecoilRoot } from 'recoil';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="mx-auto flex h-5 w-full max-w-[375px] ">
-      <RecoilRoot>
-        <Component {...pageProps} />
-      </RecoilRoot>
-    </div>
+    <RecoilRoot>
+      <Head>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+        <title>Hollang</title>
+      </Head>
+      <Component {...pageProps} />
+    </RecoilRoot>
   );
 }
