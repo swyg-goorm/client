ce76f7466b6d07a3c0f5a7c48002a5d2e56206d3
Kuesung Park <gueit214@naver.com>
2023-02-17 11:06:50 +0900

    feat : PrgressBar 컴포넌트 구현

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface ProgressBarProps {
+  order: number;
+  [key: string]: any;
+}
+export default function ProgressBar({ order }: ProgressBarProps) {
+  return (
+    <div className="h-[11px] w-[432px] rounded-[20px] bg-[#d9d9d9]">
+      <div
+        className={`h-[11px] w-[calc(36px*${order})] absolute rounded-[20px] bg-cyan-900`}
+      />
+    </div>
+  );
+}
