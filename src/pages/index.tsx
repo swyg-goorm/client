073167be007f568c9531c58aa0da5ff263ffcd84
Kuesung Park <gueit214@naver.com>
2023-02-17 10:48:56 +0900

    Revert "feat : Input 컴포넌트"
    This reverts commit 9036afecc8f6486bf3f661b2a5271727a03e4439.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
-import Input from '@components/common/Input';
 import Layout from '@components/common/layout';
 
 export default function Home() {
-  return (
-    <Layout>
-      <Input placeholder="닉네임을 입력해주세요" />
-    </Layout>
-  );
+  return <Layout>Hello</Layout>;
 }
