809285f36f1dbf62ad64d6ae8ef75856f19ce52b
Kuesung Park <gueit214@naver.com>
2023-02-17 09:22:19 +0900

    feat : isBack 제거

--- src/components/common/layout.tsx
+++ src/components/common/layout.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 interface LayoutProps {
-  isBack?: boolean;
   children?: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex h-screen w-screen justify-center bg-cyan-900">
-      <div className="relative h-full w-full max-w-[450px] overflow-y-scroll bg-white px-[20px] scrollbar-hide">
+    <div className="flex h-screen w-screen justify-center bg-cyan-900 ">
+      <div className="relative h-full w-full max-w-[450px] overflow-y-scroll bg-white px-[20px] scrollbar-hide ">
         {children}
       </div>
     </div>
