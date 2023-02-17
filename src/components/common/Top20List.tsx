f8fb129948802578bc2b4fa2d1e742f9e3871393
Kuesung Park <gueit214@naver.com>
2023-02-17 10:32:03 +0900

    feat : Top20List 컴포넌트 구현

---
+++ src/components/common/Top20List.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface Top20ListProps {
+  ranking: number;
+  title: string;
+  count: number;
+  [key: string]: any;
+}
+
+export default function Top20List({ ranking, title, count }: Top20ListProps) {
+  return (
+    <div className="flex h-[70px] w-[410px] items-center justify-between rounded-[30px] bg-sub px-[40px] text-title3 text-[#545454]">
+      <span>{ranking}위</span>
+      <span>{title}</span>
+      <span>{count}명</span>
+    </div>
+  );
+}
