bed062c7d85cee6e9b97ee392f0ce3baa738247f
Kuesung Park <gueit214@naver.com>
2023-02-17 09:19:10 +0900

    feat : tailwind config 적용 & 아이콘 삭제(중복)

--- src/components/common/layout.tsx
+++ src/components/common/layout.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import arrow_back from '@public/images/arrow_back.svg';
-import logo from '@public/images/logo_main.svg';
 
 interface LayoutProps {
   isBack?: boolean;
   children?: React.ReactNode;
 }
 
-export default function Layout({ isBack = false, children }: LayoutProps) {
-  const router = useRouter();
+export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex h-screen w-screen justify-center bg-slate-50">
+    <div className="flex h-screen w-screen justify-center bg-cyan-900">
       <div className="relative h-full w-full max-w-[450px] overflow-y-scroll bg-white px-[20px] scrollbar-hide">
         {children}
       </div>
