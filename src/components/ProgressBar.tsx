1cb932e2febe7788e0c945fa60bcdb8925b08aa8
Kuesung Park <gueit214@naver.com>
2023-02-19 08:56:48 +0900

    design : 너비 수정

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -6,7 +6,7 @@ interface ProgressBarProps {
 }
 export default function ProgressBar({ order }: ProgressBarProps) {
   return (
-    <div className="h-[11px] w-[432px] rounded-[20px] bg-[#d9d9d9]">
+    <div className="h-[11px] w-full rounded-[20px] bg-[#d9d9d9]">
       <div
         className={`absolute h-[11px] w-[calc(36px*${order})] rounded-[20px] bg-cyan-900`}
       ></div>
