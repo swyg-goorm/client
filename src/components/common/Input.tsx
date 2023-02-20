144902e336c5eef5b3b785f6da7412742a49c124
Kuesung Park <gueit214@naver.com>
2023-02-20 22:04:33 +0900

    feat : 타입 & props 추가

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 
 interface InputProps {
   placeholder: string;
+  nickname: string;
+  setNickname: (nickname: string) => void;
 }
-export default function Input({ placeholder }: InputProps) {
-  const [nickname, setNickname] = useState('');
+export default function Input({
+  placeholder,
+  nickname,
+  setNickname,
+}: InputProps) {
   const handleNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNickname(e.target.value);
   };
@@ -12,9 +17,10 @@ export default function Input({ placeholder }: InputProps) {
     <input
       type="text"
       placeholder={placeholder}
-      className="h-[71px] w-[410px] rounded-[30px] border-none bg-sub pl-[16px] placeholder:text-gray-6"
+      className="h-[71px] w-full rounded-[20px] border-none bg-sub pl-[16px] text-[16px] placeholder:text-gray-6"
       onChange={handleNickname}
       value={nickname}
+      autoFocus
     />
   );
 }
