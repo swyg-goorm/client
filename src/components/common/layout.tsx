9c77feef6150b1ebcb1d541c81a8b2fb056cddb3
Kuesung Park <gueit214@naver.com>
2023-02-20 22:04:25 +0900

    refactor : 불필요 코드 제거

--- src/components/common/layout.tsx
+++ src/components/common/layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex h-screen w-screen justify-center bg-cyan-900 ">
-      <div className="relative h-full w-full max-w-[450px] overflow-y-scroll bg-white px-[20px] scrollbar-hide ">
+      <div className="relative h-full w-full max-w-[450px] overflow-y-scroll bg-white px-[20px]">
         {children}
       </div>
     </div>
