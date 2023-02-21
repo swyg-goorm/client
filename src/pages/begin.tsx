9cbabfb788ec9115b5eec5a808300f813b5e593f
Kuesung Park <gueit214@naver.com>
2023-02-22 08:58:28 +0900

    feat : Button, TopBar 컴포넌트 삽입

--- src/pages/begin.tsx
+++ src/pages/begin.tsx
@@ -2,13 +2,14 @@ import React, { ChangeEvent, useState } from 'react';
 import { useRouter } from 'next/router';
 import Layout from '@components/common/layout';
 import Input from '@components/common/Input';
+import Button from '@components/common/Button';
+import TopBar from '@components/common/TopBar';
 
 const reg = /[가-힣]{1,3}$/;
 
 export default function Begin() {
   const router = useRouter();
   const [nickname, setNickname] = useState<string>('');
-  const [nicknameError, setNicknameError] = useState<string>('');
   const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
     // API 전송
@@ -17,9 +18,10 @@ export default function Begin() {
 
   return (
     <Layout>
+      <TopBar isBackButton />
       <div className="mt-[108px]">
         <p className="text-[30px]">홀랑에 빠질 준비 되셨나요?</p>
-        <p className="mt-[12px] text-[18px] text-black1">
+        <p className="text-black1 mt-[12px] text-[18px]">
           인공지능이 당신을 홀랑 빠질 취미로 추천해줄거에요
         </p>
       </div>
@@ -36,12 +38,9 @@ export default function Begin() {
             </span>
           )}
         </div>
-        <button
-          className="fixed inset-x-0 bottom-[40px] m-auto flex h-[95px] w-full max-w-[430px] cursor-pointer items-center justify-center rounded-[30px] bg-cyan-100 text-[24px]"
-          type="submit"
-        >
-          홀랑 테스트 하러 가기
-        </button>
+        <div className="fixed inset-x-0 bottom-[5px] m-auto max-w-[410px]">
+          <Button>홀랑 테스트 하러 가기</Button>
+        </div>
       </form>
     </Layout>
   );
