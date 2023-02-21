1de7978810e8a117d32da46f4cbabe29f4e5f2ba
Kuesung Park <gueit214@naver.com>
2023-02-21 23:09:17 +0900

    feat : rest 추가

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 interface ProgressBarProps {
   order: number;
+  [key: string]: any;
 }
 export default function ProgressBar({ order, ...rest }: ProgressBarProps) {
   return (
-    <div className="h-[11px] w-full rounded-[20px] bg-gray-3">
+    <div className="h-[11px] w-full rounded-[20px] bg-gray-3" {...rest}>
       <div
         style={{ width: `${(order / 12) * 100}%` }}
         className="h-full rounded-[20px] bg-main-4"
