9a7f4a55f544b69ae5ad606a77aab8ca2a2a1a0b
Kuesung Park <gueit214@naver.com>
2023-02-21 09:32:43 +0900

    fix : 스타일 수정

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -7,7 +7,10 @@ interface ProgressBarProps {
 export default function ProgressBar({ order }: ProgressBarProps) {
   return (
     <div className="h-[11px] w-full rounded-[20px] bg-gray-8">
-      <div className={`h-[11px]  w-${order}/12 rounded-[20px] bg-main-4`}></div>
+      <div
+        style={{ width: `${(order / 12) * 100}%` }}
+        className="bg-main-4` h-full rounded-[20px]"
+      ></div>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Layout from '@components/common/layout';
+import ProgressBar from '@components/common/ProgressBar';
 import TopBar from '@components/common/TopBar';
 
 export default function Home() {
