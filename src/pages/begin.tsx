f12a2d8623295c5365ebbcb29b333f84d67168d3
Kuesung Park <gueit214@naver.com>
2023-02-21 09:11:14 +0900

    feat : 정규표현식 수정

--- src/pages/begin.tsx
+++ src/pages/begin.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import Layout from '@components/common/layout';
 import Input from '@components/common/Input';
 
-const reg = /[^가-힣]{1,3}/;
+const reg = /[가-힣]{1,3}$/;
 
 export default function Begin() {
   const router = useRouter();
@@ -19,8 +19,8 @@ export default function Begin() {
   return (
     <Layout>
       <div className="mt-[108px]">
-        <p className="text-[30px] ">홀랑에 빠질 준비 되셨나요?</p>
-        <p className="text-[18px] text-black1">
+        <p className="text-[30px]">홀랑에 빠질 준비 되셨나요?</p>
+        <p className="mt-[12px] text-[18px] text-black1">
           인공지능이 당신을 홀랑 빠질 취미로 추천해줄거에요
         </p>
       </div>
@@ -31,10 +31,12 @@ export default function Begin() {
             placeholder="닉네임을 세글자 이내로 입력해주세요"
             setNickname={setNickname}
           />
-          {nicknameError && <span>한글로 세글자 이내까지 입력가능해요</span>}
+          {!reg.test(nickname) && (
+            <span className='text-warning block mt-[7px] ml-[20px]'>한글로 세글자 이내까지 입력가능해요</span>
+          )}
         </div>
         <button
-          className="fixed  inset-x-0 bottom-[40px] m-auto flex h-[95px] w-full max-w-[430px] cursor-pointer items-center justify-center rounded-[30px] bg-cyan-100 text-[24px]"
+          className="fixed inset-x-0 bottom-[40px] m-auto flex h-[95px] w-full max-w-[430px] cursor-pointer items-center justify-center rounded-[30px] bg-cyan-100 text-[24px]"
           type="submit"
         >
           홀랑 테스트 하러 가기
