14eb3db24e200a0b75cfb1749184f6132ed80162
Kuesung Park <gueit214@naver.com>
2023-02-21 15:06:02 +0900

    refactor : config 변경에 따른 변경

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 interface ProgressBarProps {
   order: number;
 }
-export default function ProgressBar({ order }: ProgressBarProps) {
+export default function ProgressBar({ order, ...rest }: ProgressBarProps) {
   return (
-    <div className="h-[11px] w-full rounded-[20px] bg-gray-8">
+    <div className="h-[11px] w-full rounded-[20px] bg-gray-3">
       <div
         style={{ width: `${(order / 12) * 100}%` }}
-        className="bg-main-4` h-full rounded-[20px]"
+        className="h-full rounded-[20px] bg-main-4"
       ></div>
     </div>
   );
