f2ae1d5655fbad5f9ed5074d664e9dda8f2ec857
Kuesung Park <gueit214@naver.com>
2023-02-22 11:53:09 +0900

    test

--- src/components/common/Top20List.tsx
+++ src/components/common/Top20List.tsx
@@ -11,7 +11,7 @@ export default function Top20List({ ranking, title, count }: Top20ListProps) {
     <div className="flex h-[70px] w-full items-center justify-between rounded-[30px] bg-sub-1 px-[40px] text-title3 text-gray-8">
       <span>{ranking}위</span>
       <span>{title}</span>
-      <span>{count}명</span>
+      <span>{count} 명</span>
     </div>
   );
 }
