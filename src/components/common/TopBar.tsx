ddbb0dc288c353c65a253bb97a5aba398fd38a8e
moseung <tmdah900@naver.com>
2023-02-22 12:14:04 +0900

    test

--- src/components/common/TopBar.tsx
+++ src/components/common/TopBar.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Back from '@public/static/back.svg';
-import MainLogo from '@public/static/main_logo.svg';
 
 interface ITopBar {
   isBackButton?: boolean;
@@ -9,14 +7,7 @@ interface ITopBar {
 export default function TopBar({ isBackButton }: ITopBar) {
   return (
     <div className="flex w-full justify-between p-6">
-      {isBackButton ? (
-        <button>
-          <Back />
-        </button>
-      ) : (
-        <div></div>
-      )}
-      <MainLogo></MainLogo>
+      {isBackButton ? <button></button> : <div></div>}
       <div></div>
     </div>
   );
