18c2c2882160a1bc87ee7b27d4497be084a7b77e
Kuesung Park <gueit214@naver.com>
2023-02-22 12:03:24 +0900

    hotfix : svgr 제거

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config) => {
-    // 아래를 추가합니다.
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
