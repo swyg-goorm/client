d7ce01b458aae6a292d2b945feca6db5d789ac98
moseung <tmdah900@naver.com>
2023-02-23 10:45:49 +0900

    fix: cors에러해결 -cors에러 해결 -mock데이터 생성

--- src/pages/api/getUserCount.ts
+++ src/pages/api/getUserCount.ts
@@ -1,6 +1,62 @@
 import { AxiosResponse } from 'axios';
 import { baseInstance } from './instance';
 
+const userQuestionsMockData = {
+  user: {
+    name: '하이',
+  },
+  testResponseDetail: [
+    {
+      questionNumber: 1,
+      answerNumber: 2,
+    },
+    {
+      questionNumber: 2,
+      answerNumber: 1,
+    },
+    {
+      questionNumber: 3,
+      answerNumber: 1,
+    },
+    {
+      questionNumber: 4,
+      answerNumber: 2,
+    },
+    {
+      questionNumber: 5,
+      answerNumber: 1,
+    },
+    {
+      questionNumber: 6,
+      answerNumber: 1,
+    },
+    {
+      questionNumber: 7,
+      answerNumber: 2,
+    },
+    {
+      questionNumber: 8,
+      answerNumber: 1,
+    },
+    {
+      questionNumber: 9,
+      answerNumber: 1,
+    },
+    {
+      questionNumber: 10,
+      answerNumber: 2,
+    },
+    {
+      questionNumber: 11,
+      answerNumber: 1,
+    },
+    {
+      questionNumber: 12,
+      answerNumber: 1,
+    },
+  ],
+};
+
 export const getUserCount = (): Promise<AxiosResponse<number>> => {
   return baseInstance.get('/test-responses/count');
 };
--- src/pages/api/instance.ts
+++ src/pages/api/instance.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const baseInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_END_POINT,
-  withCredentials: true,
   timeout: 5000,
 });
 
