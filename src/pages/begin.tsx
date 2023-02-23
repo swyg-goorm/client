1810c0fb065e8acfa69260d8a17a47326de23d0e
Kuesung Park <gueit214@naver.com>
2023-02-23 13:34:23 +0900

    feat : innerWidth에따른 동적 크기 변화

--- src/pages/begin.tsx
+++ src/pages/begin.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Layout from '@components/common/layout';
 import Input from '@components/common/Input';
@@ -11,6 +11,7 @@ const reg = /[가-힣]{1,3}$/;
 export default function Begin() {
   const router = useRouter();
   const [nickname, setNickname] = useState<string>('');
+  const [innerWidth, setInnerWidth] = useState<number>(0);
   const [modal, setModal] = useState<boolean>(false);
   const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -18,44 +19,62 @@ export default function Begin() {
     if (nickname === '') {
       setModal(true);
     }
-
     console.log(nickname);
+  };
+
+  useEffect(() => {
+    setInnerWidth(window.innerWidth);
+    function handleResize() {
+      setInnerWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const setClassNameByWidth = (width: number) => {
+    return innerWidth > 450 ? 'w-[410px]' : `w-[calc(${width}px-40px)]`;
   };
 
   return (
     <Layout>
-      {modal && (
-        <Modal
-          message="닉네임을 입력해주세요"
-          onCloseModal={() => {
-            setModal(false);
-          }}
-        />
-      )}
-      <TopBar isBackButton />
-      <div className="mt-[108px]">
-        <p className="text-[30px] font-[400]">홀랑에 빠질 준비 되셨나요?</p>
-        <p className="text-black1 mt-[12px] text-[18px]">
-          인공지능이 홀랑 빠질 취미로 안내할거에요
-        </p>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div className="mt-[264px]">
-          <Input
-            nickname={nickname}
-            placeholder="닉네임을 세글자 이내로 입력해주세요"
-            setNickname={setNickname}
+      <div className="relative">
+        {modal && (
+          <Modal
+            message="닉네임을 입력해주세요"
+            onCloseModal={() => {
+              setModal(false);
+            }}
           />
-          {!reg.test(nickname) && nickname.length > 0 && (
-            <span className="mt-[8px] ml-[20px] block text-warning">
-              한글로 세글자 이내까지 입력가능해요
-            </span>
-          )}
+        )}
+        <TopBar isBackButton />
+        <div className="mt-[108px]">
+          <p className="text-[30px] font-[400]">홀랑에 빠질 준비 되셨나요?</p>
+          <p className="text-black1 mt-[12px] text-[18px]">
+            인공지능이 홀랑 빠질 취미로 안내할거에요
+          </p>
         </div>
-        <div className="fixed inset-x-0 bottom-[10px] m-auto max-w-[410px]">
-          <Button>홀랑 테스트 하러 가기</Button>
-        </div>
-      </form>
+        <form onSubmit={handleSubmit} className="relative">
+          <div className="mt-[264px]">
+            <Input
+              nickname={nickname}
+              placeholder="닉네임을 세글자 이내로 입력해주세요"
+              setNickname={setNickname}
+            />
+            {!reg.test(nickname) && nickname.length > 0 && (
+              <span className="mt-[8px] ml-[20px] block text-warning">
+                한글로 세글자 이내까지 입력가능해요
+              </span>
+            )}
+          </div>
+          <div
+            className={`fixed inset-x-0 bottom-[10px] m-auto ${setClassNameByWidth(
+              innerWidth,
+            )}`}
+          >
+            <Button>홀랑 테스트 하러 가기</Button>
+          </div>
+        </form>
+      </div>
     </Layout>
   );
 }
