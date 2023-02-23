602b1c754b377300c2ce1a65c59e6682403927f0
Kuesung Park <gueit214@naver.com>
2023-02-23 15:12:16 +0900

    feat : Card 보완

--- src/components/common/result/Card.tsx
+++ src/components/common/result/Card.tsx
@@ -10,9 +10,11 @@ export default function Card() {
   const router = useRouter();
   return (
     <div className="relative flex h-[328px] w-[265px]  justify-center rounded-[30px] border border-main-3 bg-main-1 ">
-      <div className="flex h-[200px] items-center">
+      <div className="flex h-[250px] flex-col justify-center gap-[20px]">
         <ExampleHobby />
+        <p className="text-[24px] text-gray-6">서핑</p>
       </div>
+
       <div className="absolute bottom-[24px] w-full px-[16px]">
         <Button>더 알아보기</Button>
       </div>
