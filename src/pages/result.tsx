f9168fff63600efde8617fb7650884c71313681f
Kuesung Park <gueit214@naver.com>
2023-02-24 09:21:15 +0900

    refactor : 단위 변환

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -6,32 +6,31 @@ import Card from '@components/common/result/Card';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Pagination } from 'swiper';
 
 export default function Result() {
   const router = useRouter();
   return (
     <div className="flex flex-col items-center text-center">
-      <section className="mt-[24px]">
-        <p className="text-[24px] text-main-4">
+      <section className="mt-[1.5rem]">
+        <p className="text-[1.5rem] text-main-4">
           <span className="text-main-3">꼬꼬꼬</span>님의 홀랑 유형
         </p>
-        <ExampleType className="mt-[16px]" />
-        <p className="mt-[16px] text-[24px] text-gray-7">
+        <ExampleType className="mt-[1rem]" />
+        <p className="mt-[1rem] text-[1.5rem] text-gray-7">
           상상을 현실로 행동형
         </p>
-        <p className="mt-[32px] w-[275px] text-[18px] leading-[30px] text-gray-8">
+        <p className="mt-[2rem] w-[17.1875rem] text-[1.125rem] leading-[1.875rem] text-gray-8">
           감정이 풍부하고 배려심이 많은 사막에서 물을 파는 발굴형은 혼자서도 즐
         </p>
       </section>
-      <section className="mt-[32px]">
-        <p className="text-[24px] text-main-4">
+      <section className="mt-[2rem]">
+        <p className="text-[1.5rem] text-main-4">
           <span className="text-main-3">꼬꼬꼬</span>님의 홀랑 취미
         </p>
-        <p className="mt-[16px] text-[18px] text-gray-7">
+        <p className="mt-[1rem] text-[18px] text-gray-7">
           버튼을 눌러 자세히 둘러봐요!
         </p>
-        <article className="mt-[24px]">
+        <article className="mt-[1.5rem]">
           <Card />
         </article>
         <Swiper className="mySwiper" observer={true} observeParents={true}>
@@ -45,13 +44,13 @@ export default function Result() {
           <SwiperSlide>Slide 8</SwiperSlide>
           <SwiperSlide>Slide 9</SwiperSlide>
         </Swiper>
-        <p className="mt-[32px]">스와이퍼</p>
+        <p className="mt-[2rem]">스와이퍼</p>
       </section>
       <section className="mt-[48px] w-full ">
-        <p className="text-[24px] text-main-4">나와 찰떡인 홀랑 유형</p>
+        <p className="text-[1.5rem] text-main-4">나와 찰떡인 홀랑 유형</p>
         <p className="mt-[20px]">아래 버튼을 눌러 알아봐요!</p>
         <p className="mt-[20px]">이미지</p>
-        <div className="mt-[24px]">
+        <div className="mt-[1.5rem]">
           <div>
             <Button
               onClick={() => {
@@ -61,7 +60,7 @@ export default function Result() {
               공유하기
             </Button>
           </div>
-          <div className="mt-[16px]">
+          <div className="mt-[1rem]">
             <Button
               property="secondary"
               onClick={() => {
