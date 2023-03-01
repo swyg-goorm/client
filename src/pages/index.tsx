7ba2a313a9280469a4f6c7e3afee8e84a3311a01
moseung <tmdah900@naver.com>
2023-03-01 17:04:41 +0900

    feat: 터치 잠금해제 기능 구현 -기본 함수로 구현 -관련 svg추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import MainCharacter from '@public/static/main_character.svg';
 import { useQuery } from 'react-query';
 import { getUserCount } from './api/getUserCount';
 import { useRouter } from 'next/router';
+import Forward from '@public/static/forward.svg';
 
 export default function Home() {
   const TAPBAR_HEIGHT = 84;
@@ -19,26 +20,65 @@ export default function Home() {
     }, 2000);
   }, [data, isSuccess]);
 
-  let startX: number = 0;
-  let startY: number;
-  let moveX: number = 0;
-  let moveY: number;
+  const [unlocked, setUnlocked] = useState(false);
+  const [x, setX] = useState(0);
+  const [dragging, setDragging] = useState(false);
+  const LAYOUT_PADDING = 20;
+  const TOUCH_BUTTON_SIZE = 80;
+  const BUTTON_PADDING = 12;
 
-  const handleDragStart = (e: any) => {
-    startX = e.pageX;
-    startY = e.pageY;
+  const handleMouseDown = (e: any) => {
+    setDragging(true);
+    setX(e.clientX);
   };
 
-  const handleDragMove = (e: any) => {
-    moveX = e.pageX - startX;
-    if (moveX !== 0) {
-      setDragStartPoint(moveX - window.innerWidth / 2 + 205 - 39);
+  const handleMouseMove = (e: any) => {
+    if (dragging) {
+      if (e.touches) {
+        return setX(
+          e.touches[0].clientX -
+            LAYOUT_PADDING -
+            TOUCH_BUTTON_SIZE / 2 -
+            BUTTON_PADDING,
+        );
+      }
+      setX(e.clientX);
     }
-    moveY = e.target.clientLeft - startY;
   };
 
-  const handleDragEnd = (e: any) => {
-    console.log('끝', e);
+  const handleMouseUp = (e: any) => {
+    setDragging(false);
+    const PC_SPARE_VALUE = 30;
+    const MOBILE_SPARE_VALUE = 15;
+    if (e.touches) {
+      if (
+        x >
+        window.innerWidth -
+          LAYOUT_PADDING * 2 -
+          TOUCH_BUTTON_SIZE -
+          BUTTON_PADDING * 2 -
+          MOBILE_SPARE_VALUE
+      ) {
+        setUnlocked(true);
+        return router.push('/begin');
+      } else {
+        return setX(0);
+      }
+    }
+    if (
+      x >
+      window.innerWidth / 2 +
+        225 -
+        LAYOUT_PADDING -
+        TOUCH_BUTTON_SIZE / 2 -
+        BUTTON_PADDING -
+        PC_SPARE_VALUE
+    ) {
+      setUnlocked(true);
+      return router.push('/begin');
+    } else {
+      setX(0);
+    }
   };
 
   return (
@@ -60,22 +100,37 @@ export default function Home() {
         >
           슬라이더를 밀어 입장하기!
         </p>
-
         <div
-          onClick={() => router.push('/begin')}
-          className="relative mb-[16px] flex h-[100px] w-full items-center rounded-[50px] bg-main-2 py-[10px] px-[12px]"
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+          onMouseLeave={handleMouseUp}
+          onTouchStart={handleMouseDown}
+          onTouchMove={handleMouseMove}
+          onTouchEnd={handleMouseUp}
+          className="slider relative mb-[16px] flex h-[100px] w-full items-center rounded-[50px] bg-main-2 py-[10px] px-[12px]"
         >
           <button
-            draggable
-            // onTouchEnd={handleDragEnd}
-            // onTouchMove={handleDragMove}
-            // onTouchStart={handleDragStart}
-            // style={{ left: dragStartPoint }}
-            onDragEnd={handleDragEnd}
-            onDrag={handleDragMove}
-            onDragStart={handleDragStart}
-            className="absolute h-[4.6875rem] w-[4.6875rem] cursor-pointer rounded-full bg-main-4"
-          ></button>
+            style={{
+              transform: `translateX(${
+                x > 0
+                  ? window.innerHeight >= 450
+                    ? x - window.innerWidth / 2 + 225 - 25
+                    : x
+                  : 0
+              }px)`,
+            }}
+            className={`h-[80px] w-[80px] cursor-pointer rounded-full bg-main-3`}
+          >
+            {unlocked ? '슬라이드 잠금 해제됨' : '슬라이드 해제'}
+          </button>
+          {!dragging && (
+            <>
+              <Forward />
+              <Forward />
+              <Forward />
+            </>
+          )}
         </div>
 
         <button
