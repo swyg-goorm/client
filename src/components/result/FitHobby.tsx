4d791cb500eac0213a230a41983e824b64bfe046
Kuesung Park <gueit214@naver.com>
2023-03-01 12:06:41 +0900

    refactor : 단위 변환

--- src/components/result/FitHobby.tsx
+++ src/components/result/FitHobby.tsx
@@ -16,14 +16,14 @@ export default function FitHobby({ fitHobbyTypes }: FitHobbyProps) {
 
   return (
     <div>
-      <p className="py-5 text-[24px] text-main-4">나와 찰떡인 홀랑 유형</p>
+      <p className="py-5 text-[1.5rem] text-main-4">나와 찰떡인 홀랑 유형</p>
       <section className="flex">
         {fitHobbyTypes.map((fitHobbyType: FitHobbyType, index: number) => (
           <div
             className="flex w-full flex-col items-center border-b border-gray-5"
             key={fitHobbyType.id}
           >
-            <div className="rounded-[30px] bg-gray-2 p-3">
+            <div className="rounded-[1.875rem] bg-gray-2 p-3">
               <Image
                 alt="fit-type"
                 src={fitHobbyType.imageUrl}
@@ -48,13 +48,13 @@ export default function FitHobby({ fitHobbyTypes }: FitHobbyProps) {
               height={180}
               className="mt-10 mb-5"
             />
-            <p className="text-[22px] text-gray-7">
+            <p className="text-[1.375rem] text-gray-7">
               꼬꼬꼬님과 잘 맞는 유형을 선택해봐요!
             </p>
           </div>
         ) : (
           <div className="flex flex-col items-center">
-            <p className="py-[60px] text-[24px] text-main-3">
+            <p className="py-[3.75rem] text-[1.5rem] text-main-3">
               {selectedType.name}
             </p>
             <Image
@@ -63,7 +63,7 @@ export default function FitHobby({ fitHobbyTypes }: FitHobbyProps) {
               width={180}
               height={180}
             />
-            <p className="mt-[28px] text-[18px] leading-[25px]">
+            <p className="mt-[1.75rem] text-[1.125rem] leading-[1.5625rem]">
               {selectedType.description}
             </p>
           </div>
