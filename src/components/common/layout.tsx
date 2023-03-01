63b917afc683391dc94f98b080e8ef3bb1c43ab1
moseung <tmdah900@naver.com>
2023-03-01 17:06:45 +0900

    fix: 레이아웃 maxWidth 수정

--- src/components/common/layout.tsx
+++ src/components/common/layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex h-screen w-screen justify-center bg-main-4 ">
-      <div className="relative h-full w-full max-w-[37rem] overflow-y-scroll bg-gray-0 px-5 ">
+      <div className="relative h-full w-full max-w-[450px] overflow-y-scroll bg-gray-0 px-5 ">
         <TopBar></TopBar>
         {children}
       </div>
