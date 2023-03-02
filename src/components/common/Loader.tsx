26b8758f3496ac1ce1d154074eac1d7ff39a7409
Kuesung Park <gueit214@naver.com>
2023-03-02 09:20:32 +0900

    feat : 닉네임 추가

--- src/components/common/Loader.tsx
+++ src/components/common/Loader.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
 const LOADING_IMAGE_SRC = `${process.env.NEXT_PUBLIC_API_CLOUD}/images/etc/loading.gif`;
+const nickname = localStorage.getItem('nickname');
 
 export default function Loader() {
   return (
     <div className="flex h-[48rem] flex-col items-center justify-center">
       <Image alt="loading" src={LOADING_IMAGE_SRC} width={300} height={300} />
       <p className="mt-6 text-[1.375rem] leading-8 text-gray-6">
-        인공지능이 000님을 위한
+        인공지능이 {nickname}님을 위한
         <br /> 최적의 취미를 찾고있어요!
       </p>
     </div>
