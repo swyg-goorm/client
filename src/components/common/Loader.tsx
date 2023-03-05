c751d78b3af22b983054e8e334b8db9e5fde8085
Kuesung Park <gueit214@naver.com>
2023-03-05 09:25:09 +0900

    feat : 로더 개선

--- src/components/common/Loader.tsx
+++ src/components/common/Loader.tsx
@@ -10,7 +10,7 @@ export default function Loader() {
   }, []);
 
   return (
-    <div className="flex h-[48rem] flex-col items-center justify-center">
+    <div className="z-10 flex h-[48rem] flex-col items-center justify-center">
       <div className=" h-[16rem] w-[16rem] items-center justify-center overflow-hidden rounded-full bg-main-4">
         <div className="mt-[2.6rem] h-[10rem] w-[800rem] overflow-hidden">
           <Image
