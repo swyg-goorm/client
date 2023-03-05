f8551fd03cf73ce27e59904deaef3caf91ce836b
Kuesung Park <gueit214@naver.com>
2023-03-05 12:43:37 +0900

    feat : validation 추가

--- src/pages/begin.tsx
+++ src/pages/begin.tsx
@@ -13,13 +13,6 @@ export default function Begin() {
   const [modal, setModal] = useState<boolean>(false);
   const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
-  };
-  const [innerWidth, setInnerWidth] = useState<number>(0);
-  useEffect(() => {
-    setInnerWidth(window.innerWidth);
-  }, []);
-
-  const handleClickButton = () => {
     if (!reg.test(nickname)) return;
     if (nickname === '') {
       setModal(true);
@@ -58,14 +51,8 @@ export default function Begin() {
             </span>
           )}
         </div>
-        <div className="w-[full]">
-          <Button
-            onClick={handleClickButton}
-            type="submit"
-            disabled={!reg.test(nickname)}
-          >
-            홀랑 테스트 하러 가기
-          </Button>
+        <div className="absolute left-0 right-0 bottom-[2.8125rem] m-auto w-[full]">
+          <Button type="submit">홀랑 테스트 하러 가기</Button>
         </div>
       </form>
     </div>
