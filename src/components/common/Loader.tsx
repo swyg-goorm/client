ee9bd69daa9f8b600980f4571748c49099e4e27d
Kuesung Park <gueit214@naver.com>
2023-03-08 09:12:08 +0900

    feat : 로더 색 변경

--- src/components/common/Loader.tsx
+++ src/components/common/Loader.tsx
@@ -3,9 +3,9 @@ export default function Loader() {
     <div className="flex h-screen items-center justify-center">
       <div className="grid gap-2">
         <div className="flex items-center justify-center space-x-2">
-          <div className="h-3 w-3 animate-bounce1 rounded-full bg-black"></div>
-          <div className="h-3 w-3 animate-bounce2 rounded-full bg-black"></div>
-          <div className="h-3 w-3 animate-bounce3 rounded-full bg-black"></div>
+          <div className="h-3 w-3 animate-bounce1 rounded-full bg-main-3"></div>
+          <div className="h-3 w-3 animate-bounce2 rounded-full bg-main-3"></div>
+          <div className="h-3 w-3 animate-bounce3 rounded-full bg-main-3"></div>
         </div>
       </div>
     </div>
