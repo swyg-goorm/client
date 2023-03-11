5e08d7b519cfeb07f145d83fb9730246181d1803
Kuesung Park <gueit214@naver.com>
2023-03-11 15:28:53 +0900

    design : 로딩 화면 디자인 수정 반영

--- src/components/common/ResultLoader.tsx
+++ src/components/common/ResultLoader.tsx
@@ -10,11 +10,11 @@ export default function ResultLoader() {
   }, []);
 
   return (
-    <div className=" z-50 flex h-[48rem] flex-col items-center justify-center  bg-white text-center">
+    <div className="z-50 flex h-[60rem] flex-col items-center justify-center bg-white text-center">
       <div className="absolute top-0">
         <TopBar />
       </div>
-      <div className=" h-[16rem] w-[16rem] overflow-hidden rounded-full bg-main-4 ">
+      <div className="h-[16rem] w-[16rem] overflow-hidden rounded-full bg-main-4 ">
         <div className="relative mt-[2.6rem] h-[10rem] w-[200rem] overflow-hidden">
           <Image
             alt="loading"
@@ -26,7 +26,7 @@ export default function ResultLoader() {
         </div>
       </div>
 
-      <p className="mt-6 text-[1.375rem] leading-8 text-gray-6">
+      <p className="mt-[3.5rem] text-[1.375rem] leading-8 text-gray-6">
         인공지능이 {nickname}님을 위한
         <br /> 최적의 취미를 찾고있어요!
       </p>
