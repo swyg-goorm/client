9f7e83be9c24c5a5d12a25125ada6307141fe847
Kuesung Park <gueit214@naver.com>
2023-03-14 21:36:05 +0900

    refactor : 크기 및 위치 수정

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -16,10 +16,10 @@ export default function Modal({ message, onCloseModal }: ModalProps) {
       />
       <div className="absolute inset-0 z-20 m-auto flex h-[10.625rem] max-w-[25.625rem]  flex-col items-center justify-evenly rounded-[30px] bg-white text-xl font-normal text-gray-7">
         <p className="mt-[0.9375rem]">{message}</p>
-        <div className="mt-4 w-[14.5rem]">
+        <div className="mt-4 flex w-[14.5rem] items-center justify-center">
           <button
             onClick={onCloseModal}
-            className="flex w-full cursor-pointer items-center justify-center rounded-[1.25rem] bg-main-2 py-[1.25rem] text-lg font-normal text-gray-8 ease-in hover:bg-main-4 disabled:cursor-not-allowed"
+            className="flex h-[3.5rem] w-[11.4375rem]  cursor-pointer items-center justify-center rounded-[1.25rem] bg-main-2 py-[1.25rem] text-lg font-normal text-gray-8 ease-in hover:bg-main-4 disabled:cursor-not-allowed"
           >
             확인
           </button>
